a169b737bcfc5bdf78bfedd7e85621cc95a7dbe9
Foundups <info@foundups.com>
2026-08-14 03:27:50 +0900

    test(reddog): inject assurance reservation store
    Worker-Lane: REDDOG_PROGRESSIVE_POLICY_CHAIN_REGRESSION_PHASE1
    
    Slice: REDDOG_PROGRESSIVE_POLICY_CHAIN_REGRESSION_PHASE1

--- modules/communication/moltbot_bridge/tests/test_reddog_main_openclaw_signed_worker_claim_loop_preflight.py
+++ modules/communication/moltbot_bridge/tests/test_reddog_main_openclaw_signed_worker_claim_loop_preflight.py
@@ -72,7 +72,10 @@ from modules.communication.moltbot_bridge.src.reddog_signed_worker_openclaw_queu
     build_reddog_signed_worker_queue_loop_runner_from_env,
 )
 from modules.communication.moltbot_bridge.tests.test_reddog_signed_worker_dispatch_task_executor import _FakeEnvDraftPrRunner
-from modules.communication.moltbot_bridge.tests.reddog_resident_queue_test_helpers import configure_signed_worker_claim_test_authority
+from modules.communication.moltbot_bridge.tests.reddog_resident_queue_test_helpers import (
+    FakeAssuranceReservationStore,
+    configure_signed_worker_claim_test_authority,
+)
 from modules.communication.moltbot_bridge.src.reddog_resident_queue_binding_profile import (
     PROFILE_SIGNED_0102_BOUNDED_CODE_FUSION_WORKTREE, PROFILE_SIGNED_0102_BOUNDED_CODE_FUSION_WORKTREE_DRAFT_PR_PATTERN_MEMORY,
     resident_queue_materializer_mode,
@@ -1225,6 +1228,7 @@ def test_main_resident_control_loop_profile_runtime_completes_socket_signed_queu
 
     def _fixed_policy_time_bootstrap(**kwargs: object):
         kwargs["now_iso"] = BOOTSTRAP_NOW
+        kwargs["assurance_reservation_store"] = FakeAssuranceReservationStore()
         return real_serial_bootstrap(**kwargs)
 
     monkeypatch.setattr(
