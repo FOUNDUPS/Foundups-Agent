f5af077ede2f215c93f5b8e9456ebdf90e98b7c3
UnDaoDu <info@foundups.com>
2026-09-09 23:55:20 +0900

    feat(esingularity): expose JHR assessment hook

---
+++ modules/foundups/esingularity/jhr/__init__.py
@@ -0,0 +1,11 @@
+"""Japan Hyperscaler Report research lane."""
+
+from .agent import Assessment, EvidenceItem, assessment_due, maybe_start_background, run_once
+
+__all__ = [
+    "Assessment",
+    "EvidenceItem",
+    "assessment_due",
+    "maybe_start_background",
+    "run_once",
+]
